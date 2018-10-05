df909fb1d5bb37fb13bcbabd6595ebc88fd40af4
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-10-05 10:06:32 +0100

    Fixed looking up color8

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,8 @@ cat $HOME/.cache/wal/sequences
 
 set -x PATH $PATH $HOME/.gem/ruby/2.5.0/bin/ $HOME/bin $HOME/.local/bin
 
-set color8 (sed -n '8p' < $HOME/.cache/wal/colors)
+# starts with color0
+set color8 (sed -n '9p' < $HOME/.cache/wal/colors)
 
 set -x TERMINAL termite
 set -x POLYBAR_RAMP_0 \%\{F"$color8"\}━━━━━━━\%\{F-\}
