5b6265f399ee3d44a602819972a83af5e5f9e446
Paul Sonkoly <sonkoly.pal@gmail.com>
2021-03-29 18:20:51 +0100

    highlighted yank (via neovim features), better trail highlight

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,2 +1,22 @@
 vim.o.termguicolors = true
+
+-- this has to be before the colorscheme is chosen
+vim.api.nvim_exec([[
+augroup ColourScheme
+  autocmd!
+  autocmd ColorScheme * highlight link ExtraWhitespace ErrorMsg
+  autocmd BufWinEnter * match ExtraWhitespace /\s\+$\| \+\ze\t/
+augroup END
+]], false)
+
+vim.cmd('highlight ExtraWhitespace ctermbg=red guibg=red')
+
 vim.cmd('colorscheme iceberg')
+
+vim.api.nvim_exec([[
+augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
+augroup END
+]], false)
+
