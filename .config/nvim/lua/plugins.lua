f293c278105e0eefb06f0c676ea64d206f424d0d
Paul Sonkoly <sonkoly.pal@gmail.com>
2021-04-07 18:34:32 +0100

    Added more plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,18 +3,20 @@ return require('packer').startup(function()
 
   use 'cocopon/iceberg.vim'
 
-  use "norcalli/nvim-colorizer.lua"
-  use 'cossonleo/neo-smooth-scroll.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'
-  use 'onsails/lspkind-nvim'
-  use 'nvim-treesitter/nvim-treesitter'
-  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
-  use 'windwp/nvim-autopairs'
-  use 'kyazdani42/nvim-web-devicons'
-  use { 'glepnir/galaxyline.nvim', branch = 'main' }
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use 'TimUntersberger/neogit'
   use 'AndrewRadev/splitjoin.vim'
-  use 'b3nj5m1n/kommentary'
+  use 'cossonleo/neo-smooth-scroll.nvim'
+  use { 'glepnir/galaxyline.nvim', branch = 'main' }
+  use 'hrsh7th/nvim-compe'
+  use 'kyazdani42/nvim-web-devicons'
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use { '/lukas-reineke/indent-blankline.nvim', branch = 'lua' }
+  use 'neovim/nvim-lspconfig'
+  use "norcalli/nvim-colorizer.lua"
+  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'onsails/lspkind-nvim'
+  use { 'SirVer/ultisnips', requires = { 'honza/vim-snippets' } }
+  use 'TimUntersberger/neogit'
+  use "tweekmonster/startuptime.vim"
+  use 'windwp/nvim-autopairs'
 end)
