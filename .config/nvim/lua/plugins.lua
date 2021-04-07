9a6971f5b7ef0c7a41d8798c52fdc3506db94366
Paul Sonkoly <sonkoly.pal@gmail.com>
2021-04-07 18:14:46 +0100

    nvim-colorizer & neo-smooth-scroll

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,26 +1,19 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
+
   use 'cocopon/iceberg.vim'
+
+  use "norcalli/nvim-colorizer.lua"
+  use 'cossonleo/neo-smooth-scroll.nvim'
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-compe'
   use 'onsails/lspkind-nvim'
   use 'nvim-treesitter/nvim-treesitter'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-  }
+  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
   use 'windwp/nvim-autopairs'
-  use {
-    'glepnir/galaxyline.nvim',
-    branch = 'main',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    }
-  }
+  use 'kyazdani42/nvim-web-devicons'
+  use { 'glepnir/galaxyline.nvim', branch = 'main' }
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use 'TimUntersberger/neogit'
   use 'AndrewRadev/splitjoin.vim'
   use 'b3nj5m1n/kommentary'
