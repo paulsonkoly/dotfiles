e796838e28fbcc75877d455a8c892715052018d7
Paul Sonkoly <paul.sonkoly@smartpension.co.uk>
2021-04-13 13:00:23 +0100

    Added easier window movement to terminal mode

--- .config/nvim/lua/bindings.lua
+++ .config/nvim/lua/bindings.lua
@@ -5,5 +5,9 @@ map('n', '<C-h>', '<C-w>h', opts)
 map('n', '<C-j>', '<C-w>j', opts)
 map('n', '<C-k>', '<C-w>k', opts)
 map('n', '<C-l>', '<C-w>l', opts)
+map('t', '<C-h>', '<C-\\><C-N><C-w>h', opts)
+map('t', '<C-j>', '<C-\\><C-N><C-w>j', opts)
+map('t', '<C-k>', '<C-\\><C-N><C-w>k', opts)
+map('t', '<C-l>', '<C-\\><C-N><C-w>l', opts)
 
 -- vim.g.mapleader = '#'
